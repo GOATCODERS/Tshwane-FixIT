18e50875976e4e383cdc7eb2f69c1586adac62c9
Thokozani Masanabo <thokozani26sngalela@gmail.com>
2025-03-18 23:55:25 +0200

    added the model for auth app

--- tshwane_fixit/authentication/models.py
+++ tshwane_fixit/authentication/models.py
@@ -1,3 +1,54 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('role', 'admin')  # Make sure superuser is always an admin
+        extra_fields.setdefault('is_staff', True)  # Superusers must have is_staff=True
+        extra_fields.setdefault('is_superuser', True)  # Superusers must have is_superuser=True
+
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('resident', 'Resident'),
+        ('employee', 'Employee'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='resident')
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name  # You likely meant to return the name, not the user.
+
+
+class Technician(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_available = models.BooleanField(default=True)
+    expertise = models.CharField(max_length=255)
+    rating = models.IntegerField(null=True, blank=True)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username}'
